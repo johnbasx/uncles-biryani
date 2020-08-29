6f0e94cf6b7f6963b67107ea2183052101c35651
JohnSkY <johnbaswork@gmail.com>
2020-08-29 10:52:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/unclesbriyani-logo.svg";
+import "./App.css";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1>
+          Uncle's <code>Biryani</code>
+        </h1>
+        <p>Sorry, website is under development</p>
+        <div className='social-links'>
+          <a
+            className='App-link'
+            href='https://www.instagram.com/uncles.biryani/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaInstagram className='social-items' />
+          </a>
+
+          <a
+            className='App-link'
+            href='https://wa.me/7085054073'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaWhatsapp className='social-items' />
+          </a>
+        </div>
       </header>
     </div>
   );
